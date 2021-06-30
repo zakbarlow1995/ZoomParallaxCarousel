cbc74a6737910c0497ddf865f833c481ea50d854
zakbarlow1995 <zak@stormideas.com>
2021-06-30 10:35:10 +0100

    Initial Commit

---
+++ ZoomParallaxCarousel/ZoomParallaxCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoomParallaxCarouselApp.swift
+//  ZoomParallaxCarousel
+//
+//  Created by Zak Barlow on 30/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct ZoomParallaxCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
